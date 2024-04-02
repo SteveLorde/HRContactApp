9c89b519c77d2a034edbf2ffd33ffce1906ed1e1
SteveLorde <mostafa.maher98@gmail.com>
2024-04-02 17:59:44 +0200

    created a State Context (called MainContext for now)

---
+++ src/Utilities/StateContext/StateContext.ts
@@ -0,0 +1,6 @@
+import {createContext} from "react";
+
+export const MainContext = createContext({
+    variable1: "",
+    variable2: ""
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {MainContext} from "./Utilities/StateContext/StateContext.ts";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <MainContext.Provider value={{variable1: "lol", variable2: "lol2"}}>
+          <App />
+      </MainContext.Provider>
   </React.StrictMode>,
 )
