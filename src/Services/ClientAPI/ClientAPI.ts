d2674767f72d6e8fca422a3bd486d967ec244a7c
SteveLorde <mostafa.maher98@gmail.com>
2024-04-04 20:47:13 +0200

    enabled semicolon lint rule (to check semicolons), created semi-placeholder Call method in ClientAPI.ts

--- src/Services/ClientAPI/ClientAPI.ts
+++ src/Services/ClientAPI/ClientAPI.ts
@@ -1,17 +1,18 @@
 import * as process from "process";
 import axios from "axios";
+import {CallOrder} from "../../Data/Models/CallOrder.ts";
 
 export default class ClientAPI {
 
-    gotoClientId : string = process.env.CLIENTID as string
-    mainURL : string = "https://api.goto.com/"
+    gotoClientId : string = process.env.CLIENTID as string;
+    mainURL : string = "https://api.goto.com/";
 
     constructor() {
     }
 
-    async Call(telphonenumber : number) {
-
-        const response = await axios.post(`${this.mainURL}/calls/v2/calls`).then(res => res.data)
+    async Call(telphonenumber : string) {
+        const newCall : CallOrder = {autoAnswer: false, dialString: telphonenumber, from: {lineId: ""}, phoneNumberId: ""};
+        const response = await axios.post(`${this.mainURL}/calls/v2/calls`, newCall).then(res => res.data);
     }
 
 }
