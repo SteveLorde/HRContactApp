acf7e449598dfba41f94d3d37c042ef8bfea8ac8
SteveLorde <mostafa.maher98@gmail.com>
2024-04-04 20:54:23 +0200

    renamed ClientAPI.ts to GoToClientAPI.ts

--- src/Services/GoToClientAPI/GoToClientAPI.ts
+++ src/Services/GoToClientAPI/GoToClientAPI.ts
@@ -2,7 +2,7 @@ import * as process from "process";
 import axios from "axios";
 import {CallOrder} from "../../Data/Models/CallOrder.ts";
 
-export default class ClientAPI {
+export default class GoToClientAPI {
 
     gotoClientId : string = process.env.CLIENTID as string;
     mainURL : string = "https://api.goto.com/";
@@ -14,5 +14,7 @@ export default class ClientAPI {
         const newCall : CallOrder = {autoAnswer: false, dialString: telphonenumber, from: {lineId: ""}, phoneNumberId: ""};
         const response = await axios.post(`${this.mainURL}/calls/v2/calls`, newCall).then(res => res.data);
     }
+
+
 
 }
