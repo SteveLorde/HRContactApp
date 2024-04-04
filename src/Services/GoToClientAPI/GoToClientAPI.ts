333f83f06754022a5bb0444cacd06d87def61c03
SteveLorde <mostafa.maher98@gmail.com>
2024-04-04 23:52:39 +0200

    more work on ClientAPI service, added several other config variables for GoTo API

--- src/Services/GoToClientAPI/GoToClientAPI.ts
+++ src/Services/GoToClientAPI/GoToClientAPI.ts
@@ -4,15 +4,28 @@ import {CallOrder} from "../../Data/Models/CallOrder.ts";
 
 export default class GoToClientAPI {
 
+    gotoClientSecret : string = process.env.CLIENTSECRET as string;
     gotoClientId : string = process.env.CLIENTID as string;
-    mainURL : string = "https://api.goto.com/";
+    gotoAccessToken : string = "";
+    gotoRedirectURL : string = process.env.REDIRECTURL as string;
+    gotoAuthURL : string = `https://authentication.logmeininc.com/oauth/authorize?client_id={${this.gotoClientId}&response_type=code&redirect_uri={${this.gotoRedirectURL}`;
+    gotoMainAPIURL : string = "https://api.goto.com/";
 
     constructor() {
     }
 
+    //Initialize Client API
+    async Initialize() {
+        this.gotoAccessToken = await this.GetAccessToken();
+    }
+
+    async GetAccessToken() {
+        return await axios.get(this.gotoAuthURL).then(res => res.data);
+    }
+
     async Call(telphonenumber : string) {
         const newCall : CallOrder = {autoAnswer: false, dialString: telphonenumber, from: {lineId: ""}, phoneNumberId: ""};
-        const response = await axios.post(`${this.mainURL}/calls/v2/calls`, newCall).then(res => res.data);
+        const response = await axios.post(`${this.gotoMainAPIURL}/calls/v2/calls`, newCall).then(res => res.data);
     }
 
 
